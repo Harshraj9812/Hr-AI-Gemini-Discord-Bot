1258d0b536f9e1d7ee5f22e7b732111404cea1fc
Harshraj9812 <harshraj9812@gmail.com>
2024-07-17 19:40:01 +0530

    Updated the Models and now Image vision is working

--- gemini.js
+++ gemini.js
@@ -9,7 +9,7 @@ async function runGeminiPro (prompt,index) {
   // Access your API key as an environment variable (see "Set up your API key" above)
   const genAI = new GoogleGenerativeAI(geminiApiKeys[index]);
   // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
@@ -32,14 +32,14 @@ async function runGeminiVision(prompt,path,mimeType,index) {
   // Access your API key as an environment variable (see "Set up your API key" above)
   const genAI = new GoogleGenerativeAI(geminiApiKeys[index]);
   // For text-and-image input (multimodal), use the gemini-pro-vision model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
   const imageParts = [
     fileToGenerativePart(path, mimeType),
   ];
   const result = await model.generateContent([prompt, ...imageParts]);
   const response = await result.response;
   const text = response.text();
-  // console.log(text);
+  console.log(text);
   return text;
 }
 
